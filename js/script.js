5de8d2c487c42b12d2fa28d546cc2ae8da5c0496
mikaelrev <mikael.myrvang@gmail.com>
2021-06-19 13:54:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -65,9 +65,38 @@ nameContainer.innerHTML = greeting;
 
 
 // Question 5
+function printListItems(list) {
+    for(let i = 0; i < list.length; i++)
+    console.log(list[i]);
+}
 
-
+printListItems(sports);
 
 
 // Question 6
 
+function createGames(gamesList){
+    
+    let html = ""; 
+
+    for(let i = 0; i < gamesList.length; i++) {
+
+        let releaseYear = "Unknown year";
+
+        if(gamesList[i].released) {
+            releaseYear = gamesList[i].released;
+        }
+
+        html += `<div class="game"
+                <h5>${gamesList[i].name}</h5>
+                <p>Released: ${releaseYear}</p>
+            </div>`
+    }
+
+    return html;
+}
+
+const newHTML = createGames(games);
+
+const gameContainer = document.querySelector(".game-container");
+gameContainer.innerHTML = newHTML;
