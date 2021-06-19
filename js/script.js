249e9870e0499f09fdd93b9860f5bfb36503b51c
mikaelrev <mikael.myrvang@gmail.com>
2021-06-19 13:45:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,3 +13,61 @@ const games = [
         released: 2020
     }
 ];
+
+
+// Question 1
+
+function printSeason(season) {
+    console.log(season);
+}
+
+printSeason("summer");
+
+
+
+// Question 2
+
+function printNumbers(firstNumber, secondNumber) {
+    console.log("First: " + firstNumber, "Second: " + secondNumber);
+}
+
+printNumbers(2,3);
+
+
+
+// Question 3
+
+function addNumbers(firstNumber, secondNumber, thirdNumber) {
+    const result = firstNumber + secondNumber + thirdNumber;
+    return result;
+}
+
+const total = addNumbers(3,6,18);
+
+const totalContainer = document.querySelector(".total");
+
+totalContainer.innerHTML = total;
+
+
+
+// Question 4
+
+function createGreeting(name) {
+    const greeting = "Hello, my name is " + name + "."; 
+    return greeting;
+}
+
+const greeting = createGreeting("Mikael");
+
+const nameContainer = document.querySelector("#name");
+nameContainer.innerHTML = greeting;
+
+
+
+// Question 5
+
+
+
+
+// Question 6
+
